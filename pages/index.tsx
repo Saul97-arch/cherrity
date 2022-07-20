cf57dfa0d6a9034085d02fca77f29d1c05ae4472
Saul97-arch <duart.herrera@gmail.com>
2022-07-20 20:11:17 -0300

    basic visualization of projects ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,102 +1,47 @@
 import type { NextPage } from 'next'
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import xml2js from "xml2js";
 
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
-import { type } from 'os';
-
-type ImageLink = {
-  $ : {size : string};
-  url : Array<string>;
-  title : string;
-};
-
-type Image = {
-  $ : { id : string },
-  imagelink : Array<ImageLink>
-};
-
-type CountryData = {
-  iso3166CountryCode : Array<string>;
-  name : Array<string>;
-};
-
-type CountryItem = {
-  country : Array<CountryData>
-};
-
-type DataProject = {
-  $ : string; // this is the number of projects available
-  hasNext : string;
-  nextProjectId : string;
-  project : Array<Project>;
-};
-
-type Theme = {
-  theme : Array<ThemeData>
-};
-
-type ThemeData = {
-  id : Array<string>;
-  name : Array<string>;
-};
-
-type Project = {
-  active: string;
-  activities: string;
-  additionalDocumentation : string;
-  contactAddress : string;
-  contactAddress2 : string;
-  contactCity : string;
-  contactCountry : string;
-  contactUrl : string;
-  countries : Array<CountryItem>;
-  country : Array<string>;
-  funding : string;
-  goal : string;
-  id : string;
-  image : Array<Image>;
-  longTermImpact : Array<string>;
-  need : Array<String>;
-  numberOfDonations : Array<String>;
-  projectLink : Array<string>;
-  region : Array<string>;
-  status : Array<string>;
-  summary : Array<string>;
-  title : Array<string>;
-  themes : Array<Theme>
-};
-
-type CharityResponse = {
-  projects: DataProject;
-};
-
-async function getData() {
-
-  let images ;
-  const data = await axios.get<CharityResponse>("https://api.globalgiving.org/api/public/projectservice/all/projects?api_key=83bb4162-61f7-4ce6-bef1-20f702e27ad5",
-    {
-      headers: {
-        ContentType: "application/xml",
-        Accept: 'application/xml'
-      }
-    }).then(data => {
-      let parser = new xml2js.Parser();
-      parser.parseString(data.data, function (err, res : CharityResponse) {
-        // console.log(res.projects.project.at(0)?.image.at(0)?.imagelink[1].url.at(0));
-        images = res.projects.project.at(0)?.image.at(0)?.imagelink;
-      })
-    });
-
-  return images;    
-}
-
+import { useEffect, useState } from 'react'
+import { CharityResponse, Project } from './types/ProjectTypes';
 
 const Home: NextPage = () => {
+  const [image, setImage] = useState<string | undefined>("")
+  const [project, setProjectArr] = useState<Array<Project>>();
+
+  async function getData() {
+    await axios.get<CharityResponse>("https://api.globalgiving.org/api/public/projectservice/all/projects?api_key=83bb4162-61f7-4ce6-bef1-20f702e27ad5",
+      {
+        headers: {
+          ContentType: "application/xml",
+          Accept: 'application/xml'
+        }
+      }).then(data => {
+        const parser = new xml2js.Parser();
+        parser.parseString(data.data, function (err, res: CharityResponse) {
+          console.log(res.projects.project);
+          //setImage(res.projects.projectList.at(6)?.image.at(0)?.imagelink[5].url.at(0));
+          setProjectArr(res.projects.project); // TODO trocar a ordem das propriedades
+          console.log("IMAGEM AHHH " + res.projects);
+        })
+      });
+  }
+
+  useEffect(() => {
+    getData()
+  }, [image])
 
   return (
-   <button onClick={getData}>sadasd</button>
+    <div>
+      {project?.map((project, index) => {
+        return (
+          <div key={index}>
+            <p>{project.title}</p>
+            <img src={project.image.at(0)?.imagelink[3].url.at(0)} />
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
